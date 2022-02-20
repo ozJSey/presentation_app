82f1986a39e3be39808d12bf139cf59b03f0a96f
ozJSey <oz@trooptravel.com>
2022-02-20 04:05:55 +0300

    feat: added an animation library, updated based on my subjects, wrote necessary style & animation bindings

--- src/assets/presentation.js
+++ src/assets/presentation.js
@@ -1,44 +1,130 @@
 const subjects = [
   {
     "/welcome": {
-      "Hello and Welcome!": {
-        subject: "This is a route",
+      "Merhaba BAAL!": {
+        subject: "Kim ki bu?",
         style: "flip",
         texts: [
           {
             id: "asd1",
-            message: "Hello World",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            message: "Özgür Seyidoğlu",
             visible: false,
           },
           {
             id: "asd2",
-            message: "Iz Ozgur Seyidoglu",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            message: "Yazılım Müdendisi, ama dahası var tabi...",
             visible: false,
           },
           {
             id: "asd3",
-            message: "Funny what we used to pay for",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
+            message:
+              "Tam tanım: Sr. Web/Mobile Applications Frontend Development Engineer",
           },
           {
             id: "asd4",
-            message: "Instead of using microsoft products",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            message: "Bu ne demek?",
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/software": {
+      "Yazılım nedir tam olarak?": {
+        subject: "Hardware/Software",
+        style: "flip",
+        texts: [
+          {
+            id: "asd48",
+            message: "Yazılım nedir?",
+            visible: false,
+          },
+          {
+            id: "asd48",
+            message: "Yazılım dilleri ve kullanım alanları",
+            visible: false,
+          },
+          {
+            id: "asd50",
+            message: "Compiler nedir, intrepeter nedir?",
+            visible: false,
+          },
+          {
+            id: "asd49",
+            message: "Mühendis ne iş yapar, geliştirici ne iş yapar?",
+            visible: false,
+          },
+        ]
+      }
+    },
+    
+  },
+  {
+    "/software-engineering": {
+      "Yazılım Mühendisliği": {
+        subject: "Yazılım müdendisleri tam olarak ne yapıyor?",
+        style: "flip",
+        texts: [
+          {
+            id: "asd5",
+            message: "Frontend Developer | Önyüz Yazılım Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd6",
+            message: "Backend Developer | Arkayüz Yazılım Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd7",
+            message: "Full-stack Software Developer | Tam Türkçe'si maalesef",
+            visible: false,
+          },
+          {
+            id: "asd8",
+            message:
+              "Security Software Developer | Güvenlik Yazılımı Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd9",
+            message: "Cyber Security | Siber Güvenlik Uzmanı",
+            visible: false,
+          },
+          {
+            id: "asd10",
+            message: "AI Developer | Yapay Zeka Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd11",
+            message: "Game Developer | Oyun Yazılımı Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd12",
+            message: "Embedded Software Engineer | Gömülü Yazılım Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd14",
+            message:
+              "Desktop Software Engineer | Bilgisayar Yazılımı Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd15",
+            message: "Cloud Computing | Bulut Yazılımı Geliştirici",
+            visible: false,
+          },
+          {
+            id: "asd16",
+            message: "DevOps Engineering | Tam Türkçesi yine yok 🙂",
+            visible: false,
+          },
+          {
+            id: "asd17",
+            message: "SEO | Arama Motoru Optimizasyonu",
             visible: false,
           },
         ],
@@ -46,146 +132,346 @@ const subjects = [
     },
   },
   {
-    "/whoami": {
-      Presentation: {
-        subject: "This is another route!",
+    "/where-to-start": {
+      "Nereden başlamalıyım": {
+        subject: "Her hikayenin bir girişi var tabi!",
         style: "flip",
         texts: [
           {
-            id: "asd1",
-            message: "I prepared a presentation app",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            id: "asd30",
+            message: "İngilizce",
             visible: false,
           },
           {
-            id: "asd2",
-            message: "Feel free to use",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            id: "asd30",
+            message: "Klavye dahil!",
             visible: false,
           },
           {
-            id: "asd3",
-            message: "Supports all keyboard shortcuts",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            id: "asd31",
+            message: "Üniversite",
             visible: false,
           },
           {
-            id: "asd14",
-            style: "title-it",
-            message:
-              "So what about styling? Oh ye you can cheat many titles in!",
+            id: "asd32",
+            message: "Tercih",
             visible: false,
           },
           {
-            id: "asd4",
-            message:
-              "And you can style it by passing style attributes in this object",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd5",
-            message:
-              "Or you can pass it to text divs or to images to create decent views!",
-            image: {
-              source: "http://www.placekitten.com/408/287",
-              style: "Embed",
-            },
-            style: "make-bg",
-            visible: false,
-          },
-          {
-            id: "asd6",
-            message: "I will add few for my presentation",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd7",
-            message:
-              "Pages & things to say doesn't have a limit, so have at it!",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd9",
-            message: "Just fill the object as you like and code CSS for it!",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd10",
-            style: "black-bg",
-            message: "See?",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd10",
-            style: "black-bg",
-            message: "And you be like, oh no way it will extend to 3rd row :D",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd10",
-            style: "black-bg",
-            message: "Now CSS IS just showing off",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd10",
-            style: "black-bg",
-            message: "Seriously though 3 column is all we have space for",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
-            visible: false,
-          },
-          {
-            id: "asd10",
-            style: "black-bg",
-            message:
-              "This much information on a page should be enough anyways!",
-            image: {
-              source: "Something",
-              style: "Embed",
-            },
+            id: "asd32",
+            message: "Be water my friend. -Bruce Lee",
             visible: false,
           },
         ],
+      },
+    },
+  },
+  {
+    "/why-to-start": {
+      "Neden Başlamalıyım?": {
+        subject: "Bir nedeni olmalı değil mi?",
+        style: "flip",
+        texts: [
+          {
+            id: "asd30",
+            message: "Software Memes",
+            visible: false,
+          },
+          {
+            id: "asd31",
+            message: "Parası?",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Esas Nedenler",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/education": {
+      "/Eğitim kısmı": {
+        subject: "Neyin ne olduğunu bilmelisiniz!",
+        style: "flip",
+        texts: [
+          {
+            id: "asd30",
+            message: "Üniversite",
+            visible: false,
+          },
+          {
+            id: "asd31",
+            message: "Kendi kendine",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Bitmiyor...",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/companies": {
+      Şirketler: {
+        subject: "Şirketler ne yapıyor?",
+        style: "flip",
+        texts: [
+          {
+            id: "as18",
+            message: "Yazılımcası: IaaS vs PaaS vs SaaS",
+            visible: false,
+          },
+          {
+            id: "asd19",
+            message: "Software company | Yazılım Şirketi",
+            visible: false,
+          },
+          {
+            id: "asd20",
+            message: "Product company | Ürün Odaklı Şirketleri",
+            visible: false,
+          },
+          {
+            id: "asd21",
+            message: "Infrastruction company | Altyapı Şirketi",
+            visible: false,
+          },
+
+          {
+            id: "asd22",
+            message: "Startups | Girişimler",
+            visible: false,
+          },
+          {
+            id: "asd23",
+            message: "Software Agency | Yazılım Ajansı",
+            visible: false,
+          },
+          {
+            id: "asd24",
+            message: "IT Company | Bilgi Teknolojileri Şirketi",
+            visible: false,
+          },
+          {
+            id: "asd25",
+            message: "Cyber Security | Siber Güvenlik Şirketleri",
+            visible: false,
+          },
+          {
+            id: "asd26",
+            message: "Hackers | TCK 243",
+            visible: false,
+          },
+          {
+            id: "asd27",
+            message:
+              "Advertisement Software Agency | Online Reklamcılık Ajansı",
+            visible: false,
+          },
+          {
+            id: "asd28",
+            message: "E-Commerce | E-ticaret",
+            visible: false,
+          },
+          {
+            id: "asd29",
+            message: "Diğerleri",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/finding-jobs": {
+      "İş imkanları": {
+        subject: "Daha iyisi yok!",
+        style: "flip",
+        texts: [
+          {
+            id: "asd33",
+            message: "İlk iş",
+            visible: false,
+          },
+          {
+            id: "asd31",
+            message: "Maaş durumu",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Oldum dedikten sonra...",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/working-conditions-and-office-environment": {
+      "Çalışma Sartları ve ofis ortamı": {
+        subject: "Çoğu şirkette aynı şeyle karşılaşıcaksınız",
+        style: "flip",
+        texts: [
+          {
+            id: "asd33",
+            message: "Önerilerim",
+            visible: false,
+          },
+          {
+            id: "asd31",
+            message: "Hiyerarşi",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Yoğunluk",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Beklentiler",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Sunulanlar",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Remote Hybrid On-site",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Çalışma Vizesi",
+            visible: false,
+          },
+          {
+            id: "asd32",
+            message: "Stres",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/short-notes": {
+      "Bilale Anlatır Gibi": {
+        subject: "Bazı genel geçer terimler",
+        style: "flip",
+        texts: [
+          {
+            id: "asd33",
+            message: "Big Data",
+            visible: false,
+          },
+          {
+            id: "asd34",
+            message: "Internet",
+            visible: false,
+          },
+          {
+            id: "asd34",
+            message: "Web1/Web2/Web3",
+            visible: false,
+          },
+          {
+            id: "asd36",
+            message: "Cryptocurrency/BITCOIN",
+            visible: false,
+          },
+          {
+            id: "asd37",
+            message: "AR/Metaverse",
+            visible: false,
+          },
+          {
+            id: "asd38",
+            message: "NFT",
+            visible: false,
+          },
+          {
+            id: "asd38",
+            message: "Version Control/GIT",
+            visible: false,
+          },
+          {
+            id: "asd39",
+            message: "Bilgisayar çalışma prensibi ve kodun nasıl işlendiği",
+            visible: false,
+          },
+          {
+            id: "asd40",
+            message: "Quantum Computing",
+            visible: false,
+          },
+          {
+            id: "asd41",
+            message: "Documentation",
+            visible: false,
+          },
+          {
+            id: "asd42",
+            message: "IDE",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/wish-i-knew": {
+      "Benim takıldığım yerler": {
+        subject: "Tecrübelerimden bilginize arz.",
+        style: "flip",
+        texts: [
+          {
+            id: "asd43",
+            message: "Tabağınızdakini bitirin önce!",
+            visible: false,
+          },
+          {
+            id: "asd44",
+            message: "Gereksiz stres",
+            visible: false,
+          },
+          {
+            id: "asd45",
+            message: "Kendinizden harcamayın",
+            visible: false,
+          },
+          {
+            id: "asd46",
+            message: "Daha iyisi var dengesini kurmak",
+            visible: false,
+          },
+          {
+            id: "asd46",
+            message: "Çekişmeler",
+            visible: false,
+          },
+          {
+            id: "asd47",
+            message: "Yazılımla ilgili diğer herşeyin önemi",
+            visible: false,
+          },
+        ],
+      },
+    },
+  },
+  {
+    "/questions": {
+      Sorular: {
+        subject: "Aklınıza hala takılan bir şey varsa!",
       },
     },
   },
